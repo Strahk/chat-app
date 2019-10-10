1091172825e85f8d5a619817f7e06dd6c4ebada9
Ismael Repir <repirismael6@gmail.com>
2019-10-10 23:21:35 +0200

    application style css

--- src/components/contactlist.js
+++ src/components/contactlist.js
@@ -4,11 +4,13 @@ import './contactlist.css';
 function ContactList(props) {
     const user = ['Charlie Johnson', 'Jeffery Richards', 'Judy Mendoza', 'Terrance Day', 'Tyler Holmes']
     return (
-        <ul>
-            {
-                user.map(member => <li key={member}>{member}</li>)
-            }
-        </ul>
+        <div className="user">
+            <ul>
+                {
+                    user.map(member => <li key={member}>{member}</li>)
+                }
+            </ul>
+        </div>
     );
 
 }
