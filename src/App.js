6248b83c4d396eb172d74f34251a81efee4da45a
Ismael Repir <repirismael6@gmail.com>
2019-10-14 17:32:28 +0200

    restructuration de contact list pour affichage correct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import Contact from './components/contact.js';
 import ContactList from './components/contactlist.js';
 
 function App() {
 
     return (
         <div className="App">
-            <Contact
-                avatar="https://randomuser.me/api/portraits/lego/3.jpg"
-                name="Cali"
-                online
-            />
-            <Contact
-                avatar="https://randomuser.me/api/portraits/lego/6.jpg"
-                name="Pelo"
-            />
-            <Contact
-                avatar="https://randomuser.me/api/portraits/lego/7.jpg"
-                name="Bob"
-                online
-            />
             <ContactList />
         </div>
     );
--- src/components/contactlist.js
+++ src/components/contactlist.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import './contactlist.css';
+import Contact from './contact';
+
+
+const user = [
+    {
+        avatar: "https://randomuser.me/api/portraits/men/78.jpg",
+        name: "LAiden Snyder",
+        online: 1,
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/women/4.jpg",
+        name: "Jenny Anderson",
+        online: 1,
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/men/21.jpg",
+        name: "Vincent Walters",
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/women/49.jpg",
+        name: "Ethel Garrett",
+        online: 1,
+    },
+
+    {
+        avatar: "https://randomuser.me/api/portraits/men/30.jpg",
+        name: "Kenneth Mitchelle",
+    },
+
+]
 
 function ContactList(props) {
-    const user = ['Charlie Johnson', 'Jeffery Richards', 'Judy Mendoza', 'Terrance Day', 'Tyler Holmes']
     return (
-        <div className="user">
-            <ul>
-                {
-                    user.map(member => <li key={member}>{member}</li>)
-                }
-            </ul>
-        </div>
-    );
-
+        <ul>
+            {user.map(item => (
+                <Contact avatar={item.avatar} name={item.name} online={item.online} />
+            ))}
+        </ul>
+    )
 }
 
 export default ContactList;
